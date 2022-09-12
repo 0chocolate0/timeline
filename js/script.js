5a80e84d19d14ed1c3490c585e5ea8ab1c03b4bb
0chocolate0 <albertnm98@gmail.com>
2022-09-12 19:04:34 -0400

    arreglando las cajas para que se vean igual y centrando los meses

--- js/script.js
+++ js/script.js
@@ -1,29 +1,7 @@
-// var swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 1,
-//   grabCursor: true,
-//   loop: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-
-// var swiper = new Swiper(".mySwiper", {
-//   pagination: {
-//     el: ".swiper-pagination",
-//     type: "fraction",
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
 
 var swiper = new Swiper(".mySwiper", {
+  cssMode: true,
+
   autoHeight: false,
   spaceBetween: 20,
   navigation: {
@@ -35,3 +13,42 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+let $etapas1 = document.querySelector(".etapas1")
+let $etapas2A = document.querySelector(".a")
+let $etapas2B = document.querySelector(".b")
+let $etapas3 = document.querySelector(".etapas3")
+document.addEventListener("click",(e)=>{
+
+
+
+   if(e.target.matches(".swiper-button-next *")){
+    $etapas1.classList.add("inv")
+    $etapas3.classList.add("inv")
+  }
+
+   if(e.target.matches(".swiper-button-next *") && $etapas2A.classList.contains("inv")){
+    $etapas2A.classList.remove("inv")
+    $etapas2B.classList.remove("inv")
+  }else if(e.target.matches(".swiper-button-next *") && !$etapas2A.classList.contains("inv")){
+    $etapas2A.classList.add("inv")
+    $etapas2B.classList.add("inv")
+    $etapas3.classList.remove("inv")
+  }else if(e.target.matches(".swiper-button-next *") && !$etapas2A.classList.contains("inv") && $etapas1.classList.contains("inv")){
+    $etapas2A.classList.add("inv")
+    $etapas2B.classList.add("inv")
+    $etapas3.classList.remove("inv")
+  }
+ 
+   if(e.target.matches(".swiper-button-prev *") && !$etapas2A.classList.contains("inv")){
+    $etapas2A.classList.add("inv")
+    $etapas2B.classList.add("inv")
+    $etapas1.classList.remove("inv")
+  }else if(e.target.matches(".swiper-button-prev *") && $etapas2A.classList.contains("inv") && !$etapas3.classList.contains("inv")){
+    $etapas2A.classList.remove("inv")
+    $etapas2B.classList.remove("inv")
+    $etapas3.classList.add("inv")
+
+}
+  
+})
